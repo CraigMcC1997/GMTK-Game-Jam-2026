13a047f252d921a7d758a4e3a7a28207777c6c17
Craig <craigmccorrisken@gmail.com>
2026-07-23 15:57:10 +0100

    Gates added. - Collect a key to unlock gate - Gate animates - Unlocking a gate removes the key from inventory

---
+++ Assets/Scripts/DoorManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    private Animator doorAnimator;
+    public KeysManager keysManager; // Reference to the KeysManager script
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        doorAnimator = GetComponent<Animator>();
+    }
+
+    void OpenDoor()
+    {
+        if (doorAnimator != null)
+        {
+            doorAnimator.SetTrigger("Open");
+            keysManager.UseKey();
+            gameObject.GetComponent<Collider2D>().enabled = false; // Disable the collider to allow passage
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (PlayerPrefs.GetInt("KeyCount", 0) > 0)
+            {
+                PlayerPrefs.SetInt("KeyCount", PlayerPrefs.GetInt("KeyCount", 0) - 1);
+                Debug.Log("Used a key to open the door. Remaining: " + PlayerPrefs.GetInt("KeyCount", 0));
+                OpenDoor();
+            }
+            else
+            {
+                Debug.Log("No keys available to open the door.");
+                return; // Exit if no keys are available
+            }
+            
+        }
+    }
+}
