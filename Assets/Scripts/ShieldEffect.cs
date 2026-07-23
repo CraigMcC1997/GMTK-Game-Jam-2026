1c6fc3d938733b285216caf4692e431aeb713f64
Craig <craigmccorrisken@gmail.com>
2026-07-23 13:38:07 +0100

    Shield now flashes when it takes damage

--- Assets/Scripts/ShieldEffect.cs
+++ Assets/Scripts/ShieldEffect.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
 
 public class ShieldEffect : MonoBehaviour
 {
@@ -30,5 +32,35 @@ public class ShieldEffect : MonoBehaviour
         // Slight size pulse
         float scale = 1 + Mathf.Sin(Time.time * pulseSpeed) * 0.03f;
         transform.localScale = baseScale * scale;
+
+        //!!!! TESTING PURPOSES ONLY, REMOVE LATER
+        if (Keyboard.current.hKey.wasPressedThisFrame)
+        {
+            ShieldHit();
+        }
+    }
+
+    public void ShieldHit()
+    {
+        StartCoroutine(ShieldHitEffect());
+    }
+
+    IEnumerator ShieldHitEffect()
+    {
+        // Flash bright
+        shieldRenderer.color = Color.white;
+
+        // Expand slightly
+        transform.localScale = baseScale * 1.2f;
+
+        yield return new WaitForSeconds(0.1f);
+
+        // Return to normal
+        transform.localScale = baseScale;
+
+        // Restore normal alpha
+        Color colour = shieldRenderer.color;
+        colour.a = minAlpha;
+        shieldRenderer.color = colour;
     }
 }
