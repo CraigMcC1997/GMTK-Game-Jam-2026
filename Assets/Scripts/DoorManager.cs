693ff915d1b17b85e2da960c6cbd825a8b2d3b07
Craig <craigmccorrisken@gmail.com>
2026-07-24 10:42:45 +0100

    Added Tileset for drawing out game map quickly The gates now show red icons for locked and turn green if you approach with a key

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class DoorManager : MonoBehaviour
 {
     private Animator doorAnimator;
     public KeysManager keysManager; // Reference to the KeysManager script
+    public List<GameObject> UnlockIcon; // Reference to the unlock icon GameObject
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         doorAnimator = GetComponent<Animator>();
+        UnlockIcon[0].GetComponent<Renderer>().material.color = Color.red; // Initial color
+        UnlockIcon[1].GetComponent<Renderer>().material.color = Color.red; // Initial color
     }
 
     void OpenDoor()
@@ -27,6 +31,9 @@ public class DoorManager : MonoBehaviour
         {
             if (PlayerPrefs.GetInt("KeyCount", 0) > 0)
             {
+                // update unlock the door icon
+                UnlockIcon[0].GetComponent<Renderer>().material.color = Color.green; // Change the color to indicate the door is unlocked
+                UnlockIcon[1].GetComponent<Renderer>().material.color = Color.green; // Change the color to indicate the door is unlocked
                 OpenDoor();
             }
             else
